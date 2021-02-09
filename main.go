bce1a76ccdc329b6ece0405d60c89598f7ea7b0a
Daniele Tricoli <eriol@mornie.org>
2021-02-09 18:02:32 +0100

    Remove the expected import from eriol.xyz

--- main.go
+++ main.go
@@ -3,7 +3,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package main // import "eriol.xyz/g-g-g-g-geil"
+package main
 
 import (
 	"fmt"
